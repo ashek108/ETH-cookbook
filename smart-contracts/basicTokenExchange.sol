3a69d444d6451722509a2a0a496058eacb1cd849
Abhishek Kumar Singh <abhishek10k8singh@gmail.com>
2026-05-01 19:15:53 +0530

    added basicTokenExchange

---
+++ smart-contracts/basicTokenExchange.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract ERC20 {
+    string public name;
+    string public symbol;
+
+    mapping(address => uint256) public balanceOf;
+    address public owner;
+    uint public decimals;
+    uint256 public totalSupply;
+
+    // owner -> spender (allowance)
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+        decimals = 18;
+
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) public virtual {
+        require(msg.sender==owner, "only owner can mint tokens");
+        totalSupply += amount;
+        balanceOf[to] += amount;
+    }
+
+    function helpTransfer(address from, address to, uint256 amount) internal returns (bool) {
+        require(balanceOf[from] >= amount, "not enough money");
+        require(to != address(0), "cannot send to zero address");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        return helpTransfer(msg.sender, to, amount);
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        if (msg.sender != from) {
+            require(allowance[from][msg.sender] >= amount, "not enough allowance");
+            allowance[from][msg.sender] -= amount;
+        }
+
+        return helpTransfer(from, to, amount);
+    }
+}
+
+// SKILLSCOIN
+contract SkillsCoin is ERC20 {
+    constructor() ERC20("SkillsCoin", "SKC") {}
+
+    function mint(address to, uint256 amount) public override {
+        totalSupply += amount;     
+        balanceOf[to] += amount;   
+    }
+}
+
+
+// RARECOIN
+contract RareCoin is ERC20 {
+
+    SkillsCoin public skillsCoin; 
+
+    constructor(address _skillsCoin) ERC20("RareCoin", "RRC") {
+        skillsCoin = SkillsCoin(_skillsCoin); 
+    }
+
+    function trade(uint256 amount) public {
+        bool ok = skillsCoin.transferFrom(
+            msg.sender,     
+            address(this),  
+            amount
+        );
+        require(ok, "transfer failed");
+
+        totalSupply += amount;
+        balanceOf[msg.sender] += amount;
+    }
+}
